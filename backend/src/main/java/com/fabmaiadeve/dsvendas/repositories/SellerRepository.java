22de5b662c7f9cec9ed19829e22a1974ade3f022
FabianoMaia <fmaiati@gmail.com>
2021-09-11 12:34:23 -0300

    Camadas

---
+++ backend/src/main/java/com/fabmaiadeve/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.fabmaiadeve.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fabmaiadeve.dsvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+
+}
