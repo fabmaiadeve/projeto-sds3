75e3ed43ebdace7745c355da633efd167f3ccd3f
FabianoMaia <fmaiati@gmail.com>
2021-09-18 20:24:02 -0300

    Integracao BarChart

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -12,29 +12,29 @@ type ChartData = {
 const DonutChart = () => {
 
     const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
-    
+
     useEffect(() => {
-        axios.get(BASE_URL + '/sales/amount-by-seller') 
-         .then(response => {
-             const data = response.data as SaleSum[];
-             const myLabels = data.map(x => x.sellerName);
-             const mySeries = data.map(x => x.soma);
+        axios.get(BASE_URL + '/sales/amount-by-seller')
+            .then(response => {
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.soma);
 
-             setChartData ({ labels: myLabels, series: mySeries });          
+                setChartData({ labels: myLabels, series: mySeries });
 
-         });
-    } , []);
+            });
+    }, []);
 
     // FORMA ERRADA
     //let chartData : ChartData = { labels: [], series: [] };
 
-    
+
 
     //const mockData = {
     //    series: [477138, 499928, 444867, 220426, 473088],
     //    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
     //}
-    
+
     const options = {
         legend: {
             show: true
